00fa2f73b6b3f7714c32bba56f11b56562657b3d
inextro <88864528+inextro@users.noreply.github.com>
2024-05-24 22:41:09 +0900

    update fine-tuning code

---
+++ term-project/fine_tuning.py
@@ -0,0 +1,77 @@
+import os
+import argparse
+
+from datasets import load_dataset
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, AdamW, Trainer
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model_name', type=str, required=True, help='[bert, dbert]')
+    parser.add_argument('-d', '--data_name', type=str, required=True, help='[yelp, sst2, ag_news, movie_review]')
+    
+    args = parser.parse_args()
+    model_name = args.model_name
+    data_name = args.data_name
+
+
+    # 데이터 불러오기
+    if data_name == 'yelp':
+        data = load_dataset('yelp_polarity')
+        train_data = data['train']
+    elif data_name == 'sst2':
+        data = load_dataset('glue', 'sst2')
+        train_data = data['train']
+    elif data_name == 'ag_news':
+        data = load_dataset('ag_news')
+        train_data = data['train']
+    elif data_name == 'movie_review':
+        pass
+    else:
+        raise ValueError('Unknown data name')
+
+
+    # 모델 불러오기
+    num_labels = len(set(data['train']['label']))
+
+    if model_name == 'bert':
+        model = AutoModelForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=num_labels)
+        tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+    elif model_name == 'dbert':
+        model = AutoModelForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=num_labels)
+        tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
+    else:
+        raise ValueError('Unknown model name')
+
+
+    # fine-tuning
+    def tokenize_function(examples):
+        if data_name == 'yelp' or data_name == 'ag_news':
+            return tokenizer(examples['text'], padding='max_length', truncation=True)
+        elif data_name == 'sst2':
+            return tokenizer(examples['sentence'], padding='max_length', truncation=True)
+        elif data_name == 'movie_review':
+            return None
+        
+    training_args = TrainingArguments(
+        output_dir='./output',
+        num_train_epochs=3
+    )
+
+    trainer = Trainer(
+        model=model, 
+        args=training_args, 
+        train_dataset=train_data.map(tokenize_function, batched=True), 
+        optimizers=(AdamW, {'lr': 5e-6})
+    )
+    trainer.train()
+
+    # fine-tuned 모델 저장
+    model_save_dir = './result/saved_model'
+    if not os.path.exists(model_save_dir):
+        os.makedirs(model_save_dir) # 저장경로가 존재하지 않으면 해당 경로 생성
+    model.save(os.path.join(model_save_dir, model_name, data_name))
+
+
+if __name__ == '__main__':    
+    main()
